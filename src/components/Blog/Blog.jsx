5c3abb5bde2e4dec4fba474496f0a15fc5d0fff4
IEEEMOSTAFA <skmostafa8888@gmail.com>
2024-07-30 10:12:23 +0600

    module 42_5 added

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,12 +1,12 @@
 import PropTypes from 'prop-types';
+import { FaBookmark } from 'react-icons/fa';
 
-
-const Blog = ({blog}) => {
+const Blog = ({blog, handleAddToBookmark}) => {
     const {title, cover,author,posted_date,author_img,reading_time,hashtags} = blog;
     return (
-        <div>
-            <img className='w-full' src={cover} alt={`Cover picture of the title ${title}`} />
-            <div className='flex justify-between'>
+        <div className='mb-20'>
+            <img className='w-full mb-8' src={cover} alt={`Cover picture of the title ${title}`} />
+            <div className='flex justify-between mb-4'>
                 <div className='flex '>
                     <img className='w-14' src={author_img} alt="" />
                     <div className='ml-6'>
@@ -15,7 +15,9 @@ const Blog = ({blog}) => {
                     </div>
                 </div>
                 <div>
-                    <span>{reading_time} min read</span>
+                    <span>{reading_time} min read </span>
+                    <button onClick={handleAddToBookmark}
+                    className='ml-2 text-purple-600 text-2xl'><FaBookmark></FaBookmark></button>
                 </div>
             </div>
             <h2 className="text-4xl">{title}</h2>
@@ -30,4 +32,4 @@ const Blog = ({blog}) => {
 Blog.propTypes = {
     blog: PropTypes.object.isRequired
 }
-export default Blog;
+export default Blog; 
